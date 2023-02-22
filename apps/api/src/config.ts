980e64cfb69e8bfd9585a6b7fd027d973df7ba80
Gabriel C. Troia <catalin.troia@gmail.com>
2023-02-22 11:59:16 +0100

    Remove all the unneded apps and api

--- apps/api/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  REDIS_URL: process.env.REDIS_URL as string,
-  APP_REVISION: process.env.APP_REVISION as string,
-};
-
-console.log('Config:', config);
--- apps/api/src/services/session/util.ts
+++
@@ -1,3 +0,0 @@
-import * as uuid from 'uuid';
-
-export const getUuid = () => uuid.v4();
