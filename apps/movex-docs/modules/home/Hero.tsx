74dda189ab961d49c0e8d80797fece1c4ec2ef16
Gabriel C. Troia <catalin.troia@gmail.com>
2023-06-01 18:27:01 +0300

    Add the readme into the docs and split it up in sections

---
+++ apps/movex-docs/modules/home/Hero.tsx
@@ -0,0 +1,17 @@
+type Props = {};
+
+export const Hero: React.FC<Props> = () => {
+  return (
+    <div
+      style={
+        {
+          // display: 'flex',
+          // flex: 1,
+          // flexDirection: 'column',
+        }
+      }
+    >
+      yep yep
+    </div>
+  );
+};
--- apps/movex-docs/theme.config.tsx
+++ apps/movex-docs/theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Movex</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/movesthatmatter/movex',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/movesthatmatter/movex',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Movex Docs',
   },
 }
 
