94bb6fc67be8dc9a156d3bb3e0e00206c1d7d1b5
Gabriel C. Troia <catalin.troia@gmail.com>
2023-06-01 11:46:28 +0300

    chore(movex-docs): :hammer: add nextra

--- apps/movex-docs/next.config.js
+++ apps/movex-docs/next.config.js
@@ -1,17 +1,7 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nrwl/next/plugins/with-nx');
-
-/**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-};
-
-module.exports = withNx(nextConfig);
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx'
+})
+ 
+module.exports = withNextra()
+ 
