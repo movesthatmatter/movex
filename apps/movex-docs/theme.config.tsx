c077daa61a4e78bde51d251e786d0ed92d4d8dc3
Gabriel C. Troia <catalin.troia@gmail.com>
2023-06-25 16:27:10 -0600

    feat(movex-docs): :sparkles: add SEO tags

--- apps/movex-docs/theme.config.tsx
+++ apps/movex-docs/theme.config.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 import { Logo } from './modules/Logo';
 import { Footer } from './components/Footer';
+import { useRouter } from 'next/router';
+
+const tags = {
+  description:
+    'Multiplayer State Synchronization. Server Authoritative. No Server Code. Secret State. Redux Api. Realtime',
+};
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
@@ -12,11 +17,24 @@ const config: DocsThemeConfig = {
     link: 'https://discord.gg/N8k447EmBh',
   },
   docsRepositoryBase: 'https://github.com/movesthatmatter/movex',
-  // footer: {
-  //   text: 'Movex',
-  // },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Movex State Sync" />
+      <meta property="og:description" content={tags.description} />
+      <meta name="apple-mobile-web-app-title" content="Movex" />
+    </>
+  ),
   footer: {
     text: <Footer />,
+  },
+  useNextSeoProps: () => {
+    const { asPath } = useRouter();
+
+    return {
+      titleTemplate: asPath === '/' ? 'Movex' : '%s | Movex',
+      description: tags.description,
+    };
   },
 };
 
