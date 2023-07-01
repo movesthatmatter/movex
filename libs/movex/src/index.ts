3191059e5b3cd5d0791b573dd01cdaaba1c9471c
Gabriel C. Troia <catalin.troia@gmail.com>
2023-07-01 15:59:07 -0600

    feat(movex): :sparkles: export toRidAsStr and toRidAsObj directly from movex

--- libs/movex/src/index.ts
+++ libs/movex/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib';
 export * from './lib/master/MovexMasterServer';
 export * from './lib/movex-store';
 export * from './lib/io-connection/io-events';
-
 export * from './lib/public-types';
 
 // @deprecated
@@ -13,3 +12,12 @@ export * as Master from './lib/master';
 export * as MovexClient from './lib/client';
 export * as MovexMaster from './lib/master';
 export * as MovexMasterLocal from './lib/master-local';
+
+export {
+  ResourceIdentifier,
+  ResourceIdentifierObj,
+  ResourceIdentifierStr,
+  AnyResourceIdentifier,
+  toResourceIdentifierObj as toRidAsObj,
+  toResourceIdentifierStr as toRidAsStr,
+} from 'movex-core-util';
