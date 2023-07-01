5bae4f6bdff37040a33b8eb20b1a314db462b5fc
Gabriel C. Troia <catalin.troia@gmail.com>
2023-07-01 17:44:43 -0600

    feat(movex-docs): :sparkles: add the Get Started article

--- apps/movex-docs/next.config.js
+++ apps/movex-docs/next.config.js
@@ -2,7 +2,7 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
 
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
+  themeConfig: './theme.config.tsx',
 })
  
 module.exports = withNx(withNextra())
