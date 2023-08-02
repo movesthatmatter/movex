c4b62da4d79a03f1b395778cd0a3a541cc50971f
Gabriel C. Troia <catalin.troia@gmail.com>
2023-08-02 02:27:43 -0400

    fix(movex-docs): :bug: improve the docs. add more pages. Fix typos

--- apps/movex-docs/modules/Logo.tsx
+++ apps/movex-docs/modules/Logo.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useCurrentTheme } from '../hooks/useCurrentTheme';
 
-export const Logo: React.FC = () => {
-  const currentTheme = useCurrentTheme();
-
-  return (
-    <img
-      width="120"
-      alt="Movex Logo"
-      src={
-        currentTheme === 'dark'
-          ? 'https://user-images.githubusercontent.com/2099521/242976595-770bccdb-9edc-4fc6-8022-86e6336d9352.png'
-          : 'https://user-images.githubusercontent.com/2099521/242976515-b42737d6-23c6-4c7f-8817-50124bbdedf8.png'
-      }
-    />
-  );
-};
+export const Logo: React.FC = () => (
+  <img
+    width="120"
+    alt="Movex Logo"
+    src={
+      useCurrentTheme() === 'dark'
+        ? '/static/logo_b_transparent@3x.png'
+        : '/static/logo_b_white_transparent@3x.png'
+    }
+  />
+);
