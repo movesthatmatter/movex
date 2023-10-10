9c566ea5e8cede9128477b400560939df836750a
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-10-10 01:10:32 +0100

    deleted unuseful comments

--- apps/movex-docs/modules/home/Hero.tsx
+++ apps/movex-docs/modules/home/Hero.tsx
@@ -50,48 +50,30 @@ export const Hero: React.FC<Props> = () => {
     <div className="">
       <div className="flex">
         <div className="flex-1">
-          <h1 className="text-6xl lg:text-7xl font-bold mb-4 lg:mb-10" style={{
-            lineHeight: '5rem',
-          }}>
-            Build{' '}
+          <h1
+            className="text-6xl lg:text-7xl font-bold mb-4 lg:mb-10"
+            style={{
+              lineHeight: '5rem',
+            }}
+          >
+            Build
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-movexBlue-400 via-indigo-500 to-purple-500 0from-movexBlue-300 sto-green-500 text-movexBlue-500">
               Mutiplayer Apps.
-            </span>{' '}
-            {/* <span
-          className={`
-          ${(currentTheme === 'dark' ? darkColors : lightColors)[indexes.color]}
-          } p-1 lg:p-2 rounded-lg`}
-          // className={`bg-orange-500 p-2 rounded-lg`}
-        >
-          {appTypes[indexes.appType]}
-        </span>{' '} */}
+            </span>
             Write only frontend code.
-            {/* <span className="text-transparent bg-clip-text bg-gradient-to-l from-movexBlue-600 to-movexBlue-300 text-movexBlue-500">Mutiplayer</span>{' '}
-        <span
-          className={`
-          ${(currentTheme === 'dark' ? darkColors : lightColors)[indexes.color]}
-          } p-1 lg:p-2 rounded-lg`}
-          // className={`bg-orange-500 p-2 rounded-lg`}
-        >
-          {appTypes[indexes.appType]}
-        </span>{' '}
-        State without hassle. */}
           </h1>
           <p className="text-xl lg:text-2xl mb-7 !leading-normal text-slate-500 inline-flex">
-            Movex is a{' '}
+            Movex is a
             <Link
               href="https://redux.js.org/introduction/getting-started"
               target="_blank"
               className="italic hover:underline"
             >
               "predictable state container*"
-            </Link>{' '}
+            </Link>
             for multiplayer applications.
             <br /> Server Authoritative by nature. No Server hassle by design.
           </p>
-          
-
-          
         </div>
       </div>
     </div>
