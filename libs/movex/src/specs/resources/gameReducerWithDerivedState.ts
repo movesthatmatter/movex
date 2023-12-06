f9d59791315e57f7014d24afec69a27afb995105
seog-jun <shong69@myseneca.ca>
2023-12-06 14:11:53 -0500

    Removed irregular whitespace

--- libs/movex/src/specs/resources/gameReducerWithDerivedState.ts
+++ libs/movex/src/specs/resources/gameReducerWithDerivedState.ts
@@ -109,7 +109,7 @@ gameReducerWithDerivedState.$canReconcileState = (
  * retained at the type level only. Think about t as 2 states, one that gets used in the reducer and gets persisted and one that is derived from it
  *
  * What this would fix is the reconciliation issues between private actions and public state, beause if this derivable fields are used then it's
- *  impsosible (without some incredible gymnasitics probably) to determine which state update is more relevant – the private (which is used now) merged
+ *  impsosible (without some incredible gymnasitics probably) to determine which state update is more relevant –the private (which is used now) merged
  *  on top of the next public, or use some sort of order of input. Even in that case if the public changes, and it changes with multiple fields at the same time
  *  but only some need to be merged, than how to decide which ones without some crazy marking or complex heuristcs.
  *
@@ -117,10 +117,10 @@ gameReducerWithDerivedState.$canReconcileState = (
  * 1. Structure the state to be as minimal as possible, each paired action (private/public) to only change the same ones and as few fields as possible
  * 2. If that doesn't suffice the $deriveState can be used to enhance the exported state
  *
- * Update. The derived state isn't really needed at the reducer level, but it could be something at MovexLevel, passing a transformer function so it can easily 
+ * Update. The derived state isn't really needed at the reducer level, but it could be something at MovexLevel, passing a transformer function so it can easily
  * read the output and work with that. On the other hand, that might not be needed as I've discovered (with only 1 test so far) that if the state is simple enough,
  * meaning that PrivateActionPair changes the same field, then it should be an issue b/c the private will always replace the temporary public. That's the main thing
- * to keep in mind – whatever gets set by the public state will be replaced by its paired private action. It's only meant to be a temporeary placeholder
+ * to keep in mind –whatever gets set by the public state will be replaced by its paired private action. It's only meant to be a temporeary placeholder
  * // Yeaaah, so think about the public pair action as a palceholder, replaceable by the private action upon reconciliation/revelation, with the private having authority
  * over replacing it. In theory the public could affect other parts of the state (more permanent, or untouched by the private action) but it probsably shouldn't
  *
