3ecd71a1864a4db47444e4ca8658a226f34d7a5f
Gabriel C. Troia <catalin.troia@gmail.com>
2024-12-23 20:25:51 +0100

    Fix a config

--- apps/movex-admin/config.ts
+++ apps/movex-admin/config.ts
@@ -1,5 +1,5 @@
 const MOVEX_ENDPOINT = process.env['NEXT_PUBLIC_MOVEX_ENDPOINT'] as string;
-const MOCK_DATA = process.env['NEXT_PUBLIC_MOCK_DATA'] === 'true';
+const MOCK_DATA = process.env['NEXT_PUBLIC_MOCK_DATA'] === 'TRUE';
 
 export const config = {
   MOVEX_ENDPOINT,
